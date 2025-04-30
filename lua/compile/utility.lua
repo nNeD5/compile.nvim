be2a1da2ebf3da87c23dc20d4397aaae456ba2b7
ned <oned.reg@gmail.com>
2025-04-30 07:03:53 +0300

    More or less working gF

--- lua/compile/utility.lua
+++ lua/compile/utility.lua
@@ -19,6 +19,31 @@ local function filter_output(data)
   return clean
 end
 
+local function edit_under_cursor_with_col()
+  local name = vim.fn.expand("<cfile>")
+  if not vim.uv.fs_stat(name) then
+    vim.notify([[ERROR: Can't find file "]] .. name .. [[ in path]], vim.log.levels.ERROR)
+    return
+  end
+  local name_len = string.len(name)
+  local cursor_col = vim.api.nvim_win_get_cursor(0)[1]
+  local line = vim.api.nvim_get_current_line()
+
+  local index_start = string.find(line, name)
+  while index_start + name_len < cursor_col do
+    index_start = string.find(line, name, index_start + 1)
+  end
+  local linenum = string.match(line, "[%s,%D](%d+)", index_start)
+  linenum = tonumber(linenum)
+  linenum = math.min(math.max(1, linenum), vim.api.nvim_buf_line_count(0) - 1)
+  print(linenum)
+
+  vim.cmd.wincmd("p")
+  vim.cmd.edit(vim.fn.fnameescape(name))
+  vim.api.nvim_win_set_cursor(0, { linenum, 0 })
+  vim.cmd.normal { 'zz', bang = true }
+end
+
 M.open_new_or_reuse_window = function(buf, win)
   -- create new buffer if invalid
   local is_buf_valid = buf and vim.api.nvim_buf_is_valid(buf)
@@ -26,6 +51,7 @@ M.open_new_or_reuse_window = function(buf, win)
     buf = vim.api.nvim_create_buf(true, true)
     baleia.automatically(buf)
     vim.keymap.set({ "n", "n" }, "gf", edit_under_cursor, { buffer = buf })
+    vim.keymap.set({ "n", "n" }, "gF", edit_under_cursor_with_col, { buffer = buf })
     if buf == 0 then
       vim.notify("Failed to open new bufdow ", vim.log.levels.ERROR)
       return nil
