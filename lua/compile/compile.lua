b5100507fb68c9e1b0236d4815bfbcb7a9fda0db
nNeD5 <oned.reg@gmail.com>
2025-10-16 21:45:56 +0300

    final

--- lua/compile/compile.lua
+++ lua/compile/compile.lua
@@ -29,7 +29,8 @@ local function get_last_cmd_from_input_buffer()
   return cmd
 end
 
-local function open_window()
+local function open_window(focus)
+  focus = focus or false
   -- close term win -> closes input win as well
   vim.api.nvim_create_autocmd("WinClosed", {
     group = group,
@@ -53,11 +54,17 @@ local function open_window()
     vim.api.nvim_buf_set_lines(input_bufid, 0, -1, false, lines)
   end
   -- open windows
+  local save_current_win = vim.api.nvim_get_current_win()
   if term_winid == nil or vim.fn.getbufinfo(term_bufid)[1].hidden == 1 then
-    term_winid = vim.api.nvim_open_win(term_bufid, true, { split = "right", win = 0 })
+    term_winid = vim.api.nvim_open_win(term_bufid, false, { split = "right", win = 0 })
     input_winid = vim.api.nvim_open_win(input_bufid, true, {win=term_winid, height=vim.o.cmdwinheight, split="below"})
     vim.keymap.set({"n", "v", "i"}, "<CR>", function() M.run_cmd(vim.api.nvim_get_current_line()) end, { buffer = true })
+    if focus == false then
+      vim.api.nvim_set_current_win(save_current_win)
+    end
   end
+
+  vim.api.nvim_set_option_value("modified", false, {scope="local", buf=term_bufid})
 end
 
 
@@ -69,24 +76,37 @@ function M.run_cmd(cmd)
   if jobid ~= nil then
     vim.notify("Kill previous job" .. cmd, vim.log.levels.WARN)
     vim.fn.jobstop(jobid)
+    vim.fn.jobwait({jobid})
   end
 
-  if term_bufid ~= nil then
-    vim.api.nvim_buf_delete(term_bufid, {force=false, unload=false})
-  end
   open_window()
   history.save_command(cmd)
-  -- vim.api.nvim_buf_set_lines(input_bufid, 0, -1, false, lines)
+  vim.api.nvim_buf_set_lines(input_bufid, 0, -1, false, history.get_history())
+  vim.api.nvim_win_set_cursor(input_winid,  {#vim.api.nvim_buf_get_lines(input_bufid, 0, -1, false), 0})
   vim.notify("Compile: " .. cmd, vim.log.levels.INFO)
+  local save_current_win = vim.api.nvim_get_current_win()
   vim.api.nvim_set_current_win(term_winid)
-  vim.api.nvim_create_autocmd("TextChanged", { buffer = term_bufid, command = "normal! G$" })
   jobid = vim.fn.jobstart("time " .. cmd, {
     pty = true,
     term = true,
+    on_stdout = function()
+      if vim.api.nvim_win_is_valid(term_winid) then
+        local last = vim.api.nvim_buf_line_count(term_bufid)
+        vim.api.nvim_win_set_cursor(term_winid, { last, 0 })
+      end
+    end,
+    on_stderr = function()
+      if vim.api.nvim_win_is_valid(term_winid) then
+        local last = vim.api.nvim_buf_line_count(term_bufid)
+        vim.api.nvim_win_set_cursor(term_winid, { last, 0 })
+      end
+    end,
     on_exit = function()
       jobid = nil
-    end
+      vim.cmd.cgetbuffer(term_bufid)
+    end,
   })
+  vim.api.nvim_set_current_win(save_current_win)
 end
 
 function M.stop_cmd()
@@ -95,7 +115,7 @@ function M.stop_cmd()
     return
   end
   vim.fn.jobstop(jobid)
-    vim.notify("Job killed", vim.log.levels.INFO)
+    vim.notify("Job killed", vim.log.levels.WARN)
 end
 
 function M.toggle_window()
@@ -103,7 +123,7 @@ function M.toggle_window()
   local is_hidden = is_hidden or not vim.api.nvim_buf_is_loaded(term_bufid)
   local is_hidden = is_hidden or vim.fn.getbufinfo(term_bufid)[1].hidden == 1
   if is_hidden then
-    open_window()
+    open_window(true)
   else
     for _, win in ipairs(vim.fn.getbufinfo(term_bufid)[1].windows) do
       vim.api.nvim_win_close(win, false)
